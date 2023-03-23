00955cb2cd53efb93d0148ff24a27c12c0bf37aa
SindhuYellenki <sindhu.allamaneni@gmail.com>
2023-03-23 04:10:18 +0000

    errorCode

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import {Route, Switch, Redirect} from 'react-router-dom'
+import {Component} from 'react'
+import Login from './component/Login'
+import Home from './component/Home'
+import NotFound from './component/NotFound'
+import Jobs from './component/Jobs'
+import JobItemDetails from './component/JobItemDetails'
+import ProtectedRoute from './component/ProtectedRoute'
+
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute exact path="/jobs" component={Jobs} />
+        <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="not-found" />
+      </Switch>
+    )
+  }
+}
+
+export default App
